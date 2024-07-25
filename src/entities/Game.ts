8b30cd0a502762cb0f830587d0b7a0db008c9b2d
Moncy Yohannan <moncyyohannan12@gmail.com>
2024-07-26 05:22:54 +0530

    Build Game Attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publishers } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publishers[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
