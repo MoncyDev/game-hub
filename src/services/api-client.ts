3e4a238e2395435d33b16046e37ee133577a78df
Moncy Yohannan <moncyyohannan12@gmail.com>
2024-09-23 22:31:15 +0530

    Update: Async API client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -20,15 +20,16 @@ class APIClient<T> {
   constructor(endpoint: string) {
     this.endpoint = endpoint;
   }
-  getAll = (config: AxiosRequestConfig) => {
-    return axiosInstance
-      .get<FetchResponse<T>>(this.endpoint, config)
-      .then((res) => res.data);
+  getAll = async (config: AxiosRequestConfig) => {
+    const res = await axiosInstance.get<FetchResponse<T>>(
+      this.endpoint,
+      config
+    );
+    return res.data;
   };
-  get = (id: number | string) => {
-    return axiosInstance
-      .get<T>(this.endpoint + "/" + id)
-      .then((res) => res.data);
+  get = async (id: number | string) => {
+    const res = await axiosInstance.get<T>(this.endpoint + "/" + id);
+    return res.data;
   };
 }
 export default APIClient;
